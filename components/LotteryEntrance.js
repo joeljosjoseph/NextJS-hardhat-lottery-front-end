2b528d64114b7a8c0ffaaa414c434cf2e046b242
joeljosjoseph <joeljosjoseph12@gmail.com>
2023-01-04 12:37:33 +0530

    Added web3uikit connect button

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { useWeb3Contract } from "react-moralis"
+
+function LotteryEntrance() {
+    // const {
+    //     data,
+    //     error,
+    //     isFetching,
+    //     isLoading,
+    //     runContractFunction: enterRaffle,
+    // } = useWeb3Contract({
+    //     abi: ,
+    //     contractAddress: ,
+    //     functionName: ,
+    //      params: {},
+    //      msgValue:
+    // })
+    return <div>LotteryEntrance</div>
+}
+
+export default LotteryEntrance
